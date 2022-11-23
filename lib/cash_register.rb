044dc9e8af4344906f41f188160b3ce6a5daff46
Emily <emelizadaniels@gmail.com>
2022-11-23 14:29:30 -0500

    commit tree 5a0515bff18de261765f766b2e5a3e1f88a1d6eb

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,23 @@
+require 'pry'
 
+class CashRegister 
+ attr_accessor :discount, :total
+#  attr_reader 
+
+
+    def initialize (discount= 0.2)
+        @discount = discount
+        @total = 0
+    end
+
+    def add_item (title, price, quantity=1)
+        @total += price * quantity
+      
+
+    end
+
+
+
+end
+cr = CashRegister.new(10)
+# binding.pry
